52f05720c2520ebb70f770aaa027e8ab7ad8efe0
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-06-12 09:41:07 +0100

    .

--- EmailServices/Email.js
+++ EmailServices/Email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_Service,
+    service: process.env.EMAIL_SERVICE,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
